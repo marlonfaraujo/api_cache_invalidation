bb867850b263a889b56e6dca2f603e3de89460a2
Marlon <marlonfda@gmail.com>
2025-06-12 23:50:49 -0300

    Redis configuration and test integration

---
+++ src/CacheInvalidation.Api/Database/RedisCacheDatabase.cs
@@ -0,0 +1,33 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+
+namespace CacheInvalidation.Api.Database
+{
+    public class RedisCacheDatabase
+    {
+        private readonly IDatabase _database;
+
+        public RedisCacheDatabase(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
+        public async Task<bool> SetAsync<T>(string key, T value, TimeSpan? expiry = null)
+        {
+            var json = JsonSerializer.Serialize(value);
+            return await _database.StringSetAsync(key, json, expiry);
+        }
+
+        public async Task<T?> GetAsync<T>(string key)
+        {
+            var json = await _database.StringGetAsync(key);
+            if (json.IsNullOrEmpty) return default;
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+        public async Task<bool> RemoverAsync(string key)
+        {
+            return await _database.KeyDeleteAsync(key);
+        }
+    }
+}
---
+++ test/CacheInvalidation.Integration/Database/RedisCacheDatabaseTest.cs
@@ -0,0 +1,33 @@
+﻿using CacheInvalidation.Api.Database;
+using CacheInvalidation.Api.Entities;
+using StackExchange.Redis;
+using System.Collections.Generic;
+
+namespace CacheInvalidation.Integration.Database
+{
+    public class RedisCacheDatabaseTest
+    {
+        private readonly RedisCacheDatabase _redis;
+
+        public RedisCacheDatabaseTest()
+        {
+            this._redis = new RedisCacheDatabase(ConnectionMultiplexer.Connect("localhost:6379,password=redis"));
+        }
+
+        [Fact(DisplayName = "Given a list of products stored in the cache when searching in redis then it should return results")]
+        public async Task Given_AListOfProductsStoredInTheCache_When_SearchingInRedis_Then_ItShouldReturnResults() 
+        {
+            int length = 5;
+            var items = new List<Product>();
+            for (int i = 0; i < length; i++)
+            {
+                var product = ProductTestData.GenerateProduct();
+                items.Add(product);
+            }
+            await this._redis.SetAsync("products", items);
+            var cachedItems = await this._redis.GetAsync<IEnumerable<Product>>("products");
+            Assert.NotNull(cachedItems);
+            Assert.NotEmpty(cachedItems);
+        }
+    }
+}
