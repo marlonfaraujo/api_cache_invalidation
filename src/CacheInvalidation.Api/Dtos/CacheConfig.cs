69d908730da505718670e04a4b8efa6f30cb55c3
Marlon <marlonfda@gmail.com>
2025-06-15 14:12:54 -0300

    Creating option via configuration to refresh cache or invalidate when there are product modifications

--- src/CacheInvalidation.Api/Dtos/CacheConfig.cs
+++ src/CacheInvalidation.Api/Dtos/CacheConfig.cs
@@ -1,6 +1,6 @@
 ﻿namespace CacheInvalidation.Api.Dtos
 {
-    public record CacheConfig(int ExpirationTimeMinutes, string ProductCacheKey)
+    public record CacheConfig(int ExpirationTimeMinutes, string ProductCacheKey, bool? ItsToRefresh = false)
     {
     }
 }
--- src/CacheInvalidation.Api/UseCases/Handlers/ProductUpdatedEventHandler.cs
+++ src/CacheInvalidation.Api/UseCases/Handlers/ProductUpdatedEventHandler.cs
@@ -11,24 +11,32 @@ namespace CacheInvalidation.Api.UseCases.Handlers
         private readonly ICacheDatabase _cacheDatabase;
         private readonly CacheConfig _cacheConfig;
         private readonly ILogger<ProductUpdatedEventHandler> _logger;
+        private readonly RefreshProductCache _refreshProductCache;
 
-        public ProductUpdatedEventHandler(ICacheDatabase cacheDatabase, IConfiguration configuration, ILogger<ProductUpdatedEventHandler> logger)
+        public ProductUpdatedEventHandler(ICacheDatabase cacheDatabase, IConfiguration configuration, ILogger<ProductUpdatedEventHandler> logger, RefreshProductCache refreshProductCache)
         {
             this._cacheDatabase = cacheDatabase;
             this._cacheConfig = configuration.GetSection("CacheConfig").Get<CacheConfig>();
             this._logger = logger;
+            this._refreshProductCache = refreshProductCache;
         }
 
-        public Task HandleAsync(ProductUpdatedEvent notification, CancellationToken cancellationToken = default)
+        public async Task HandleAsync(ProductUpdatedEvent notification, CancellationToken cancellationToken = default)
         {
-            this._cacheDatabase.RemoverAsync(this._cacheConfig.ProductCacheKey, cancellationToken);
+            if (this._cacheConfig.ItsToRefresh.GetValueOrDefault() == true)
+            {
+                await this._refreshProductCache.ExecuteAsync(cancellationToken);
+            }
+            else
+            {
+                await this._cacheDatabase.RemoverAsync(this._cacheConfig.ProductCacheKey, cancellationToken);
+            }
             var output = new OutboxMessage(notification.Product.Id,
                 nameof(ProductUpdatedEvent).ToString(),
                 JsonSerializer.Serialize(notification),
                 DateTime.UtcNow,
                 false);
             this._logger.LogInformation($"Product updated event handled: {JsonSerializer.Serialize(output)}");
-            return Task.CompletedTask;
         }
     }
 }
